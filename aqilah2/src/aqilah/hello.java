430e42e2f9260d7a6edfe68db72a65a28d197edf
user <user@LAPTOP-JAQA1I9I>
2020-10-30 23:00:03 +0800

    First commit
    Uploading file

---
+++ aqilah2/src/aqilah/hello.java
@@ -0,0 +1,52 @@
+package aqilah;
+
+public class hello {
+
+	public static void main(String[] args) {
+	
+		String name = "Siti Nur Aqilah";
+		char gender = 'F';
+		int age = 20;
+		double cgpa = 3.75;
+		String marital = "single";
+		String citizenship = "Malaysia";
+		String address = "Lot 2228 Kampung Titian Berayung KIjal Kemaman Terengganu";
+		String hp = "+601117981920";
+		String result = "3A 1B STPM";
+		String experience = "1.Restaurant Worker\n\t\t\t2.Business Network Marketing";
+		String language = "1.Malay 100%\n\t\t\t2.English 30%";
+		String skills = "JAVA Programming 50%";
+		String award = "Pelajar Cemerlang STPM 2018/2019";
+		
+		System.out.println("Name                : " + name); 
+		System.out.println();
+		System.out.println("Gender              : " + gender);
+		System.out.println();
+		System.out.println("Birthday            : 3 May 2000");
+		System.out.println();
+		System.out.println("Age                 : " + age + " years old");
+		System.out.println();
+		System.out.println("Marital Status      : " + marital);
+		System.out.println();
+		System.out.println("Citizenship         : " + citizenship);
+		System.out.println();
+		System.out.println("Home Address        : " + address);
+		System.out.println();
+		System.out.println("HP                  : " + hp);
+		System.out.println();
+		System.out.println("Exam Result         : " + result);
+		System.out.println();
+		System.out.println("CGPA                : " + cgpa);
+		System.out.println();
+		System.out.println("Work Experiences    : " + experience);
+		System.out.println();
+		System.out.println("Language            : " + language);
+		System.out.println();
+		System.out.println("Professional Skills : " + skills);
+		System.out.println();
+		System.out.println("Award               : " + award);
+		// TODO Auto-generated method stub
+          
+	}
+
+}
